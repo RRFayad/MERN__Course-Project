bd9aabb1bc8d3482d135bc74f748298d437f0de2
RRFayad <renan.r.fayad@gmail.com>
2023-04-14 07:47:37 -0300

    41. Adding a UsersList Page / Component

--- frontend/src/users/pages/Users.js
+++ frontend/src/users/pages/Users.js
@@ -1,9 +1,21 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
+  const USERS = [
+    {
+      id: "u1",
+      name: "Renan Fayad",
+      image:
+        "https://media.licdn.com/dms/image/C4E03AQH5HDkBly2nqg/profile-displayphoto-shrink_800_800/0/1655491533997?e=1686787200&v=beta&t=D8XWI1VbSkxRhrKDZx7tzEECpcobQ40La092pvK1sOw",
+      places: 3,
+    },
+  ]; // Dummy data for the front end environment (that's why its named in caps)
+
   return (
     <>
-      <h1>Users Works</h1>
+      <UsersList items={USERS} />
     </>
   );
 };
