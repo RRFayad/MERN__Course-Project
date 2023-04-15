602ce52848bed8f13a734e211a383a1fea93e803
RRFayad <renan.r.fayad@gmail.com>
2023-04-15 18:06:53 -0300

    49. Handling the Drawer State

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom";
 
 import MainHeader from "./MainHeader";
@@ -7,16 +7,27 @@ import SideDrawer from "./SideDrawer";
 import "./MainNavigation.css";
 
 const MainNavigation = (props) => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const toggleDrawerHandler = () => {
+    setDrawerIsOpen((prevState) => !prevState);
+  };
+
   return (
     <>
-      <SideDrawer>
-        <nav className="main-navigation__drawer-nav">
-          <NavLinks />
-        </nav>
-      </SideDrawer>
+      {drawerIsOpen && (
+        <SideDrawer onCloseDrawer={toggleDrawerHandler}>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      )}
 
       <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button
+          className="main-navigation__menu-btn"
+          onClick={toggleDrawerHandler}
+        >
           <span />
           <span />
           <span />
--- frontend/src/shared/components/Navigation/SideDrawer.js
+++ frontend/src/shared/components/Navigation/SideDrawer.js
@@ -6,7 +6,16 @@ import "./SideDrawer.css";
 const SideDrawer = (props) => {
   const content = <aside className="side-drawer">{props.children}</aside>;
 
-  return ReactDOM.createPortal(content, document.querySelector("#drawer-hook"));
+  const backdrop = (
+    <div className="backdrop" onClick={props.onCloseDrawer}></div>
+  );
+
+  return (
+    <>
+      {ReactDOM.createPortal(backdrop, document.querySelector("#backdrop"))}
+      {ReactDOM.createPortal(content, document.querySelector("#drawer-hook"))}
+    </>
+  );
 };
 
 export default SideDrawer;
