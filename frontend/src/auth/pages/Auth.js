db4f814be3b34837bd662e829a5dd396d95b0cae
RRFayad <renan.r.fayad@gmail.com>
2023-04-25 09:57:25 -0300

    72. Adding Signup + 'Switch Mode' Button

--- frontend/src/auth/pages/Auth.js
+++ frontend/src/auth/pages/Auth.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Input from "../../shared/components/FormElements/Input";
 import {
   VALIDATOR_MINLENGTH,
   VALIDATOR_EMAIL,
+  VALIDATOR_REQUIRE,
 } from "../../shared/util/validators";
 import Button from "../../shared/components/FormElements/Button";
 import { useForm } from "../../shared/hooks/form-hook";
 import "./Auth.css";
 
 function NewPlace() {
-  const [formState, inputHandler] = useForm(
+  const [userHasAccount, setUserHasAccount] = useState(true);
+  const [formState, inputHandler, setFormData] = useForm(
     {
       email: {
         value: "",
@@ -30,10 +32,54 @@ function NewPlace() {
     console.log(formState.inputs);
   };
 
+  const toggleHasAccountHandler = (event) => {
+    event.preventDefault();
+    if (!userHasAccount) {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: undefined,
+        },
+        formState.inputs.email.isValid && formState.inputs.password.isValid
+      );
+    } else {
+      setFormData(
+        {
+          ...formState.inputs,
+          name: {
+            value: "",
+            isValid: false,
+          },
+        },
+        false
+      );
+    }
+    setUserHasAccount((prev) => !prev);
+  };
+
   return (
-    <form className="place-form" onSubmit={authSubmitHandler}>
-      <h2 className="place-form__title">Login Required</h2>
+    <form className="auth-form" onSubmit={authSubmitHandler}>
+      <h2 className="auth-form__title">Login Required</h2>
+      <button
+        className="auth-form__button--toggle-signup"
+        onClick={toggleHasAccountHandler}
+      >
+        {userHasAccount
+          ? "Don't have an account? Click here"
+          : "Already have an account? Click here!"}
+      </button>
       <hr />
+      {!userHasAccount && (
+        <Input
+          id="name"
+          element="input"
+          type="text"
+          label="Name"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter a name"
+          onInput={inputHandler}
+        />
+      )}
       <Input
         id="email"
         element="input"
@@ -54,7 +100,7 @@ function NewPlace() {
       />
 
       <Button type="submit" disabled={!formState.isValid}>
-        Login
+        {userHasAccount ? "Login" : "Create Account"}
       </Button>
     </form>
   );
