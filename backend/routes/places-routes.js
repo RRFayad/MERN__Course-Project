72dd153f28d3bd71c714446c68dd903f6a386cd5
RRFayad <renan.r.fayad@gmail.com>
2023-08-10 09:34:08 -0300

    90. Implementing Basic Routing & 91. Adding Place-Specific Routes

---
+++ backend/routes/places-routes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const router = express.Router();
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Budapest Parliament",
+    description: "One of the most beautifuls parliaments in the world",
+    imageUrl:
+      "https://www.historyhit.com/app/uploads/fly-images/5158752/parliament-budapest-788x537.jpg",
+    address: "Budapest, Kossuth Lajos tér 1-3, 1055 Hungria",
+    location: {
+      lat: 47.5070988,
+      lng: 19.0405461,
+    },
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((item) => item.id === placeId);
+  res.json(place);
+});
+
+module.exports = router;
