4715317209f5da341760415b699b09ee81d9f1e6
RRFayad <renan.r.fayad@gmail.com>
2023-08-30 21:05:34 -0300

    132. Creating the User Model

---
+++ api/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true }, // This property is for code eficiency
+  password: { type: String, required: true, minlength: 6 },
+  image: { type: String, required: true },
+  places: { type: String, required: true },
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("User", userSchema); // returns a constructor function -> also will create the name of the collection, lowercase and plural
